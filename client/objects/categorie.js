350bfe8d5ca7f13910a098ae4d6189a72e27aa1b
Manguo1401 <aaronleehartnell@gmail.com>
2016-10-15 10:17:43 +0200

    Je craque
    TEST

---
+++ client/objects/categorie.js
@@ -0,0 +1,17 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Categorie;
+    return {
+        setters:[],
+        execute: function() {
+            Categorie = (function () {
+                function Categorie() {
+                }
+                return Categorie;
+            }());
+            exports_1("Categorie", Categorie);
+        }
+    }
+});
+//# sourceMappingURL=categorie.js.map
